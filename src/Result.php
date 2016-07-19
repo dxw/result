c788dc7bce4d9163e51eec38c2c1c2b7c50fecc1
Tom Adams <tom@dxw.com>
2016-07-19 08:59:44 -0400

    Add ok/err convenience functions

--- src/Result.php
+++ src/Result.php
@@ -7,4 +7,14 @@ abstract class Result
     abstract public function unwrap();
     abstract public function isErr(): bool;
     abstract public function getErr(): string;
+
+    public static function ok($value): \Dxw\Result\Result
+    {
+        return new \Dxw\Result\Ok($value);
+    }
+
+    public static function err($value): \Dxw\Result\Result
+    {
+        return new \Dxw\Result\Err($value);
+    }
 }
